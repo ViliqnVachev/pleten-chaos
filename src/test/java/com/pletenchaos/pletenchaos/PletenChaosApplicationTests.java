c5c5c72f1972db8142d73932bfffa97b1ce16bdd
Viliqn Vachev <vvachev@axway.com>
2022-05-02 10:10:18 +0300

    Add tests class

--- src/test/java/com/pletenchaos/pletenchaos/PletenChaosApplicationTests.java
+++ src/test/java/com/pletenchaos/pletenchaos/PletenChaosApplicationTests.java
@@ -1,13 +1,12 @@
 package com.pletenchaos.pletenchaos;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class PletenChaosApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
