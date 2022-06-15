deb3a189783c11fa924b06e92d9d3a718de924da
Viliqn Vachev <vvachev@axway.com>
2022-06-15 16:08:03 +0300

    Fix login

--- src/main/java/com/pletenchaos/pletenchaos/config/SecurityConfig.java
+++ src/main/java/com/pletenchaos/pletenchaos/config/SecurityConfig.java
@@ -3,15 +3,12 @@ package com.pletenchaos.pletenchaos.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import com.pletenchaos.pletenchaos.model.entity.enums.RoleEnum;
-import com.pletenchaos.pletenchaos.repository.UserRepository;
-import com.pletenchaos.pletenchaos.service.impl.UserDetailsImpl;
 
 @Configuration
 public class SecurityConfig {
@@ -46,11 +43,6 @@ public class SecurityConfig {
 				.logoutSuccessUrl("/login")//
 				.invalidateHttpSession(true).deleteCookies("JSESSIONID");
 		return http.build();
-	}
-
-	@Bean
-	public UserDetailsService userService(UserRepository repo) {
-		return new UserDetailsImpl(repo);
 	}
 
 	@Bean
