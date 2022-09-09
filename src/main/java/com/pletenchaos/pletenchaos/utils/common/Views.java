6a5576f1a227e70875cf249424e8f127b7f1ddc5
Viliqn Vachev <vvachev@axway.com>
2022-09-09 13:31:52 +0300

    .

---
+++ src/main/java/com/pletenchaos/pletenchaos/utils/common/Views.java
@@ -0,0 +1,14 @@
+package com.pletenchaos.pletenchaos.utils.common;
+
+public final class Views {
+
+	public static final String LOGIN_VIEW = "login";
+
+	public static final String REGISTER_VIEW = "register";
+
+	public static final String HOME_VIEW = "index";
+
+	public static final String ADD_MATERIAL_VIEW = "addMaterial";
+
+}
+
